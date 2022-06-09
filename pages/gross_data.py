8e92209e678012bc88436ba58762eaf799461298
kebukey <107162416+kebukey@users.noreply.github.com>
2022-06-09 23:28:46 +0800

    Update gross_data.py

--- pages/gross_data.py
+++ pages/gross_data.py
@@ -3,12 +3,10 @@ import requests
 import pandas as pd
 from PIL import Image
 
-def load_css(css_file):
-    with open(css_file) as f:
-        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+
 
 def app():
-    load_css('style/style.css')
+    
     img_house=Image.open('image/房子.png')
     st.subheader('合肥二手房交易数据')
     df=pd.read_csv('链家二手房分区.csv', encoding='utf-8')
